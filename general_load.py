87deaff52c12326270fb9b5b4dc2b99495882177
Jake Dowzell <jdowzell@mun.ca>
2019-07-02 23:39:31 -0230

    Updating python file for readability and usefulness

--- general_load.py
+++ general_load.py
@@ -13,6 +13,8 @@ import numpy as np
 from sklearn import metrics
 from IPython.display import display
 
+%matplotlib inline
+
 def goldilocks_temp(t):
     if t < 273:
         return -1;
@@ -34,7 +36,20 @@ with open(csv_filename, 'r') as f:
         
 dataset = pd.read_csv(csv_filename, header = 24)
 
-goldilocks_dataset = dataset.drop(['kepid', 'koi_period', 'koi_period_err1', 'koi_prad_err1', 'koi_insol_err1',  'koi_steff', 'koi_steff_err1', 'koi_slogg', 'koi_slogg_err1', 'koi_srad_err1'], axis=1).copy()
+goldilocks_dataset = dataset.drop(['kepid',
+                                   'koi_period',
+                                   'koi_period_err1',
+                                   'koi_prad_err1',
+                                   #'koi_insol',
+                                   'koi_insol_err1', 
+                                   'koi_steff',
+                                   'koi_steff_err1',
+                                   'koi_slogg',
+                                   'koi_slogg_err1',
+                                   #'koi_srad',
+                                   'koi_srad_err1'],
+                                  axis=1).copy()
+goldilocks_dataset.head()
 
 goldilocks_dataset["gold_temp"] = list(map(goldilocks_temp, goldilocks_dataset["koi_teq"]))
 goldilocks_dataset = goldilocks_dataset.dropna()
@@ -43,5 +58,3 @@ goldX = goldilocks_dataset.drop(["kepoi_name","koi_teq","gold_temp"], axis=1)
 goldY = goldilocks_dataset['gold_temp']
 
 goldX.columns = [longnames[x] for x in goldX.columns]
-
-
