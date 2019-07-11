6b8b49abf756f530338eb752dcb4a5488a1b137c
Jake Dowzell <jdowzell@mun.ca>
2019-07-10 23:19:45 -0230

    Uploading files to ask a question

--- full_load.py
+++ full_load.py
@@ -59,3 +59,41 @@ goldX = goldilocks_dataset.drop(["kepoi_name","koi_teq","gold_temp"], axis=1)
 goldY = goldilocks_dataset['gold_temp']
 
 goldX.columns = [longnames[x] for x in goldX.columns]
+
+# Make the Datasets
+
+just_right = goldilocks_dataset[goldilocks_dataset["gold_temp"] == 0].drop("kepoi_name",axis=1).copy()
+too_hot = goldilocks_dataset[goldilocks_dataset["gold_temp"] == 1].drop("kepoi_name",axis=1).copy()
+too_cold = goldilocks_dataset[goldilocks_dataset["gold_temp"] == -1].drop("kepoi_name",axis=1).copy()
+
+# Split into X and Y
+
+jrX = just_right.drop(["koi_teq", "gold_temp"], axis=1).copy()
+jrY = just_right["gold_temp"]
+
+thX = too_hot.drop(["koi_teq", "gold_temp"], axis=1).copy()
+thY = too_hot["gold_temp"]
+
+tcX = too_cold.drop(["koi_teq", "gold_temp"], axis=1).copy()
+tcY = too_cold["gold_temp"]
+
+from sklearn.model_selection import train_test_split
+
+# Splitting the hot/cold/right arrays into test and train data
+hotX_train, hotX_test, hotY_train, hotY_test = train_test_split(thX, thY, random_state=1)
+coldX_train, coldX_test, coldY_train, coldY_test = train_test_split(tcX, tcY, random_state=1)
+rightX_train, rightX_test, rightY_train, rightY_test = train_test_split(jrX, jrY, random_state=1)
+
+# Concatenating and reordering the testing and training data
+
+# Testing
+testingX = pd.concat([hotX_test, coldX_test, rightX_test])
+testingY = pd.concat([hotY_test, coldY_test, rightY_test])
+testingX = testingX.reindex(testingX.index.sort_values())
+testingY = testingY.reindex(testingY.index.sort_values())
+
+# Training
+trainingX = pd.concat([hotX_train, coldX_train, rightX_train])
+trainingY = pd.concat([hotY_train, coldY_train, rightY_train])
+trainingX = trainingX.reindex(trainingX.index.sort_values())
+trainingY = trainingY.reindex(trainingY.index.sort_values())
